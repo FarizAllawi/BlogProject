403264f73a0cb4ae4e41d5cc0a0557da8bae3604
Fariz Allawi Ghozi <farizallawi24@gmail.com>
2020-10-09 22:07:27 +0700

    - Membuat Model Artikel - Melakukan migrations terhadap model Artikel

--- Blog/Artikel/models.py
+++ Blog/Artikel/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Artikel(models.Model):
+    judul           = models.CharField(max_length=100)
+    isi             = models.TextField()
+    kategori        = models.CharField(max_length=255)
+    published       = models.DateTimeField(auto_now_add=True)
+    updated         = models.DateTimeField(auto_now=True)
+    slug            = models.SlugField(blank=True , editable=False)
+
+    def save(self, **kwargs):
+        self.slug = slugify(self.judul)
+        return super().save(**kwargs)
+
+    def __str__():
+        return "{} - {}".format(self.id, self.judul)
